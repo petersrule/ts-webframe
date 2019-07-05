63f584cb47138dd13fa25dfc8244ba3df9d8f569
Web Dev 8 <web8@Jeremys-Mac-mini.local>
2019-07-05 16:14:58 -0600

    attribute class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Net Newsome', age: 0 });
+const user = new User({ id: 1, name: 'Charitable Shrewd', age: 101 });
+
+user.on('save', () => {
+  console.log(user);
+});
 
 user.save();
